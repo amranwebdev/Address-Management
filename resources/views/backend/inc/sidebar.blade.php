4e35573c5379d24e33316d0e2d3b2e77f81494db
amranwebdev <amran.web.dev@gmail.com>
2021-03-07 15:20:35 +0600

    Home page and Sidebar updated

--- resources/views/backend/inc/sidebar.blade.php
+++ resources/views/backend/inc/sidebar.blade.php
@@ -27,8 +27,7 @@
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
-        <li class="header">MAIN NAVIGATION</li>
-        <li class="active treeview">
+        <li class="">
           <a href="{{route('home')}}">
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
           </a>
@@ -36,53 +35,53 @@
 
         <li class="treeview {{($prefix=='/divisions')?'dropdown':''}}">
           <a href="#">
-            <i class="fa fa-files-o"></i>
+            <i class="fa fa-tags"></i>
             <span>Division</span>
-            <span class="label label-primary pull-right">4</span>
+            <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{route('divisions.view')}}" class="{{($route=='divisions.view')?'active':''}}"><i class="fa fa-circle-o"></i> View Division</a></li>
+            <li><a href="{{route('divisions.view')}}" class="{{($route=='divisions.view')?'active treeview':''}}"><i class="fa fa-circle-o"></i> View Division</a></li>
           </ul>
         </li>
         <li class="treeview {{($prefix=='/districts')?'dropdown':''}}">
           <a href="#">
-            <i class="fa fa-files-o"></i>
+            <i class="fa fa-university"></i>
             <span>District</span>
-            <span class="label label-primary pull-right">4</span>
+            <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{route('districts.view')}}" class="{{($route=='districts.view')?'active':''}}"><i class="fa fa-circle-o"></i> View District</a></li>
+            <li><a href="{{route('districts.view')}}" class="{{($route=='districts.view')?'active treeview':''}}"><i class="fa fa-circle-o"></i> View District</a></li>
           </ul>
         </li>
         <li class="treeview {{($prefix=='/upazilas')?'dropdown':''}}">
           <a href="#">
-            <i class="fa fa-files-o"></i>
+            <i class="fa fa-building"></i>
             <span>Upazila</span>
-            <span class="label label-primary pull-right">4</span>
+            <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{route('upazilas.view')}}" class="{{($route=='upazilas.view')?'active':''}}"><i class="fa fa-circle-o"></i> View Upazila</a></li>
+            <li><a href="{{route('upazilas.view')}}" class="{{($route=='upazilas.view')?'active treeview':''}}"><i class="fa fa-circle-o"></i> View Upazila</a></li>
           </ul>
         </li>
         <li class="treeview {{($prefix=='/unions')?'dropdown':''}}">
           <a href="#">
-            <i class="fa fa-files-o"></i>
+            <i class="fa fa-area-chart"></i>
             <span>Union</span>
-            <span class="label label-primary pull-right">4</span>
+            <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{route('unions.view')}}" class="{{($route=='unions.view')?'active':''}}"><i class="fa fa-circle-o"></i> View Union</a></li>
+            <li><a href="{{route('unions.view')}}" class="{{($route=='unions.view')?'active treeview':''}}"><i class="fa fa-circle-o"></i> View Union</a></li>
           </ul>
         </li>
 
         <li class="treeview {{($prefix=='/villages')?'dropdown':''}}">
           <a href="#">
-            <i class="fa fa-files-o"></i>
+            <i class="fa fa-map-marker"></i>
             <span>Village</span>
-            <span class="label label-primary pull-right">4</span>
+            <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{route('villages.view')}}" class="{{($route=='villages.view')?'active':''}}"><i class="fa fa-circle-o"></i> View Village</a></li>
+            <li><a href="{{route('villages.view')}}" class="{{($route=='villages.view')?'active treeview':''}}"><i class="fa fa-circle-o"></i> View Village</a></li>
           </ul>
         </li>
 
--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 
+<section class="content-header">
+  <h1>Dashboard</h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+  </ol>
+</section>
+
 <!-- Main content -->
 <section class="content">
     <!-- Info boxes -->
